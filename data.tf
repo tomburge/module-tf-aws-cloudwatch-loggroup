d9ba62d463bf190614ea784bcfeabdf3b25d53be
Tom Burge <tom@tomburge.org>
2023-11-02 23:40:09 -0500

    updated kms policy

--- data.tf
+++ data.tf
@@ -27,7 +27,7 @@ data "aws_iam_policy_document" "key_policy" {
       type        = "Service"
       identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
     }
-    resources = ["${aws_cloudwatch_log_group.this.arn}"]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.name}"]
     effect    = "Allow"
   }
 }
