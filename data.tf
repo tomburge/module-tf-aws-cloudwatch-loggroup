7e82f80c07c16ab7ed94019e3975e3bb466a6967
Tom Burge <tom@tomburge.org>
2023-11-02 23:58:27 -0500

    updated key policy

--- data.tf
+++ data.tf
@@ -27,7 +27,13 @@ data "aws_iam_policy_document" "key_policy" {
       type        = "Service"
       identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
     }
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*"]
-    effect    = "Allow"
+    # resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*"]
+    resources = ["*"]
+    condition {
+      test     = "ArnEquals"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.name}"]
+    }
+    effect = "Allow"
   }
 }
